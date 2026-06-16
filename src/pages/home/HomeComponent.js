06190977db4f490e2cdb556723d4a8a266077a22
Pavithren <147544651+pavi0708@users.noreply.github.com>
2026-06-16 21:36:57 +0530

    Update HomeComponent.js

--- src/pages/home/HomeComponent.js
+++ src/pages/home/HomeComponent.js
@@ -2,7 +2,7 @@ import React, { Component } from "react";
 import Header from "../../components/header/Header";
 import Greeting from "../../containers/greeting/Greeting";
 import Skills from "../../containers/skills/Skills";
-import Footer from "../../components/footer/Footer";
+
 import TopButton from "../../components/topButton/TopButton";
 
 class Home extends Component {
